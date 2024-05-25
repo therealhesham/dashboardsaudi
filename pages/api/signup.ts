8f06a9102a3b04f746b9e03469f965955b44ecd0
hesham <heshammoha231992@gmail.com>
2024-05-25 13:34:48 +0300

    commit on master

--- pages/api/signup.ts
+++ pages/api/signup.ts
@@ -7,14 +7,12 @@ const prisma = new PrismaClient()
 export default async function handler(
  
  
- 
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-const newuser = await prisma.user.create({data:{admin:req.body.admin,idnumber:req.body.idnumber,role:req.body.role}})
-if(!newuser) return res.json({error:"error creating"}) 
+   req: NextApiRequest,
+  res: NextApiResponse) {
+// const newuser = await prisma.user.create({data:{admin:req.body.admin,idnumber:req.body.idnumber,role:req.body.role}})
+// if(!newuser) return res.json({error:"error creating"}) 
 
-res.json(newuser)
+// res.json(newuser)
 
 
 
